67acdade90b31c66ae20af2d79ca98768d7053ef
elena261288 <m7656587@yandex.ru>
2020-06-08 17:24:45 +0300

    name & age

--- src/srv.py
+++ src/srv.py
@@ -2,20 +2,39 @@ import socketserver
 import os
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
+from datetime import datetime
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"port = {PORT}")
+#now = datetime.now().year
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path.startswith("/hello"):
+        if self.path == '/hello' or self.path == '/hello/':
+            msg = 'Hello, dear!'
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-Length", len(msg))
+            self.end_headers()
+            self.wfile.write(msg.encode())
+
+        elif self.path.startswith("/hello"):
             path, qs = self.path.split("?")
             qs = parse_qs(qs)
-            age = qs["age"][0]
-            born = str(2020 - int(age))
+            if "name" not in qs:
+                name = 'dear'
+            else:
+                name = qs["name"][0]
+            if "age" not in qs:
+                born = ' '
+            else:
+                age = qs["age"][0]
+                year = str(2020 - int(age))
+                born = 'You were born in the ' + year + ' year'
+
             msg = f"""
-                Hello, dear!
-                You were born in the {born} year
+                Hello, {name}!
+                {born}
                 """
 
             self.send_response(200)
