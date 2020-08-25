e29d25f7c009419f82fa77a59911fcbf600326b7
elena261288 <elenamoiseeva2612@gmail.com>
2020-08-25 14:04:30 +0300

    admin jobs

--- src/applications/job/admin.py
+++ src/applications/job/admin.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from applications.job.models import Jobs
+
+
+class JobsAdminForm(forms.ModelForm):
+    class Meta:
+        model = Jobs
+        fields = "__all__"
+        widgets ={
+            "company": forms.TextInput(),
+        }
+
+
+@admin.register(Jobs)
+class JobsAdminModel(ModelAdmin):
+    form = JobsAdminForm
