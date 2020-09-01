50b2f63f4d86e9e698f25a38555ac38f641a485b
elena261288 <elenamoiseeva2612@gmail.com>
2020-09-01 19:07:15 +0300

    sign-in, sign-out, profile, update

--- src/applications/onboarding/urls.py
+++ src/applications/onboarding/urls.py
@@ -7,6 +7,8 @@ app_name = OnboardingConfig.label
 
 urlpatterns = [
     path("profile/", views.IndexView.as_view(), name="index"),
+    path("profile-update/", views.ProfileUpdateView.as_view(), name="profile-update"),
     path("sign-in/", views.SignInView.as_view(), name="sign-in"),
     path("sign-out/", views.SignOutView.as_view(), name="sign-out"),
+    path("sign-up/", views.SignUpView.as_view(), name="sign-up"),
 ]
